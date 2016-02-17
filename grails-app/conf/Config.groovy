a6f9d66bf989bb21c72f3bc3b9ccbfb889bf752d
Uday Pratap Singh <uday.singh@tothenew.com>
2016-02-17 12:21:02 +0530

    Added log configuration

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -100,11 +100,10 @@ log4j.main = {
     // Example of changing the log pattern for the default console appender:
     //
     appenders {
-        console name: 'stdout', layout: pattern(conversionPattern: '%c{2} %m%n')
+        console name: 'stdout', layout: pattern(conversionPattern: '%d{yyy-MM-dd HH:mm:ss,SSS} %p %c{2} %m%n')
     }
-    info 'grails.app'
-    error 'grails.app',
-            'org.codehaus.groovy.grails.web.servlet',        // controllers
+
+    error 'org.codehaus.groovy.grails.web.servlet',        // controllers
             'org.codehaus.groovy.grails.web.pages',          // GSP
             'org.codehaus.groovy.grails.web.sitemesh',       // layouts
             'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
@@ -115,4 +114,8 @@ log4j.main = {
             'org.springframework',
             'org.hibernate',
             'net.sf.ehcache.hibernate'
+
+    info 'grails.app'
+
+    debug 'grails.app'
 }
