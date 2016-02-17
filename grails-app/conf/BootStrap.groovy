fb58a4b8081dccf2a55994c748451a68949299bd
Uday Pratap Singh <uday.singh@tothenew.com>
2016-02-17 11:19:44 +0530

    Added find by property

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,13 +10,14 @@ class BootStrap {
         users.each {
             getById(it.id)
         }
+        findByProperty()
     }
 
 
     List<User> createUsers() {
         List<User> users = []
         (1..10).each {
-            User user = new User(name: "user ${it}", email: "user+${it}@gmail.com", dob: new Date() - it)
+            User user = new User(name: "user ${it}", email: "user+${it}@gmail.com", dob: new Date() - it, balance: 1000 * it)
             if (user.save()) {
                 users.add(user)
                 log.info "User ${user} saved successfully"
@@ -41,6 +42,17 @@ class BootStrap {
         println "_______Getting User with Id :${id}___________"
         User user2 = User.get(id)
         println "---------------------------------------------"
+    }
+
+    void findByProperty() {
+        User user = User.findByName("user 1")
+        println "user---${user.id}---"
+        User user1 = User.findByNameIlikeAndBalance("%user%", 5000)
+        println "user1---${user1.id}---"
+        User user2 = User.findByNameLikeOrBalance("%user%", 5000)
+        println "user2---${user2.id}---"
+        User user3 = User.findByNameIlikeOrBalance("%user%", 5000, [sort: 'balance', order: 'desc'])
+        println "user3---${user3.id}---"
     }
 
     def destroy = {
